251a9ab819421e8a9efcfee30ada1de9e1537492
Alexander Shinn <ashinn4@student.cccs.edu>
2025-04-26 19:54:46 -0600

    Pushed to GitHub fixing spatial reference errors

--- FinalProject/finalproject.py
+++ FinalProject/finalproject.py
@@ -2,7 +2,6 @@ import yaml
 import arcpy
 import os
 import logging
-import sys
 from etl.GSheetsEtl import GSheetsEtl
 from datetime import datetime
 
@@ -53,24 +52,21 @@ def process(config_dict):
     load(config_dict)
     logging.debug("Exiting process method")
 
-def etl():
+def etl(config_dict):
     logging.info("Starting West Nile Virus Simulation")
     logging.debug("Entering etl method")
-    config_dict = setup()
     process(config_dict)
     logging.debug("Exiting etl method")
 
 # --- GIS Functions ---
 
-def SpatialReference(aprx_path):
-    # You can check if layers are in the correct spatial reference here
+def SpatialReference():
+    aprx = arcpy.mp.ArcGISProject(r"C:\\Users\\Owner\\Documents\\GIS Programming\\westnileoutbreak\\WestNileOutbreak\\WestNileOutbreak.aprx")
     map_doc = aprx.listMaps()[0]
-    for layer in map_doc.listLayers():
-        if layer.isFeatureLayer:
-            desc = arcpy.Describe(layer)
-            if desc.spatialReference.factoryCode != 3743:
-                print(f"Layer {layer.name} is not in UTM Zone 13.")
+    colorado_north = arcpy.SpatialReference(26953)
+    map_doc.spatialReference = colorado_north
     aprx.save()
+    print(f"Spatial reference set to: {colorado_north.name}")
 
 def buffer(layer_name, buf_dist, config_dict):
     output_buffer_layer_path = os.path.join(config_dict.get('output_folder'), f"buf_{layer_name}.shp")
@@ -81,7 +77,7 @@ def buffer(layer_name, buf_dist, config_dict):
         raise FileNotFoundError(f"Input Features '{layer_name}' do not exist.")
 
 def intersect(buffer_layer_list, config_dict):
-    lyr_intersect = input("Enter the name for the intersect output layer name: ")
+    lyr_intersect = input("Enter the name for the intersect output layer name: ").strip()
     lyr_intersect_path = os.path.join(config_dict.get('gdb_path'), lyr_intersect)
     arcpy.analysis.Intersect(buffer_layer_list, lyr_intersect_path, "ALL")
     return lyr_intersect_path
@@ -116,8 +112,8 @@ def spatial_join(Building_Addresses, lyr_intersect, config_dict):
     )
     return join_layer_path
 
-def count_addresses(join_layer_path):
-    return int(arcpy.management.GetCount(join_layer_path).getOutput(0))
+def count_addresses(layer_path):
+    return int(arcpy.management.GetCount(layer_path).getOutput(0))
 
 def count_avoided_addresses(addresses_layer, erased_layer):
     avoided_layer = os.path.join(arcpy.env.workspace, "Avoided_Addresses")
@@ -130,16 +126,12 @@ def count_avoided_addresses(addresses_layer, erased_layer):
     )
     return int(arcpy.management.GetCount(avoided_layer).getOutput(0))
 
-def rendering_definition_query(layer):
-    layer.definitionQuery = ""
-
 def add_to_project(layer_path, config_dict):
     aprx = arcpy.mp.ArcGISProject(os.path.join(config_dict.get('proj_dir'), "WestNileOutbreak.aprx"))
     map_doc = aprx.listMaps()[0]
     layer = map_doc.addDataFromPath(layer_path)
     if not layer:
         raise RuntimeError(f"Failed to add layer {layer_path} to the project")
-    rendering_definition_query(layer)
     aprx.save()
     return layer
 
@@ -151,7 +143,7 @@ def rendering():
             layer.visible = True
     aprx.save()
 
-def exportMap(config_dict):
+def export_map(config_dict):
     aprx = arcpy.mp.ArcGISProject("CURRENT")
     layout = aprx.listLayouts()[0]
     output_pdf = os.path.join(config_dict.get('output_folder'), "WestNileOutbreak.pdf")
@@ -162,12 +154,12 @@ def exportMap(config_dict):
 if __name__ == '__main__':
     config_dict = setup()
     arcpy.env.parallelProcessingFactor = "100%"
-    etl()
-    proj_path = os.path.join(config_dict.get('proj_dir'), "WestNileOutbreak.aprx")
-    aprx = arcpy.mp.ArcGISProject(proj_path)
-    SpatialReference(proj_path)
+
+    etl(config_dict)
+    SpatialReference()
 
     buffer_layer_list = ["Mosquito_Larval_Sites", "Wetlands", "Lakes_and_Reservoirs", "OSMP_Properties"]
+
     for layer in buffer_layer_list:
         if arcpy.Exists(layer):
             input_distance = input(f"Enter the buffer distance for {layer} in feet: ").strip()
@@ -191,5 +183,6 @@ if __name__ == '__main__':
 
     add_to_project(joined_layer_path, config_dict)
     rendering()
-    exportMap(config_dict)
+    export_map(config_dict)
+
     print("All operations completed successfully.")
